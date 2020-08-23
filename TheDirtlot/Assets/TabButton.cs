45b07892e7b7c1122f2fa3ada8b6a768ab9307be
Rich Moll <drise13@gmail.com>
2020-08-23 11:17:11 -0500

    Added tab control

---
+++ TheDirtlot/Assets/TabButton.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class TabButton : MonoBehaviour, IPointerEnterHandler, IPointerClickHandler, IPointerExitHandler
+{
+    public Image BackgroundImage;
+    public TabController TabController;
+
+    public TabPanel Panel;
+
+    public bool IsPrimary;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        TabController.OnTabSelected(this);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        TabController.OnTabEnter(this);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        TabController.OnTabExit(this);
+    }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        BackgroundImage = GetComponent<Image>();
+        TabController.Subscribe(this);
+    }
+
+    // Update is called once per frame
+    private void Update() { }
+}
---
+++ TheDirtlot/Assets/TabController.cs
@@ -0,0 +1,60 @@
+using System.Collections.Generic;
+using System.Linq;
+
+using UnityEngine;
+
+public class TabController : MonoBehaviour
+{
+    public Sprite ActiveSprite;
+    public Sprite HoverSprite;
+    public Sprite IdleSprite;
+
+    public TabButton SelectedButton;
+
+    public List<TabButton> TabButtons = new List<TabButton>();
+
+
+    public void Subscribe(TabButton tabButton)
+    {
+        TabButtons.Add(tabButton);
+    }
+
+    public void OnTabEnter(TabButton tabButton)
+    {
+        ResetTabs();
+
+        if (tabButton != SelectedButton)
+        {
+            tabButton.BackgroundImage.sprite = HoverSprite;
+        }
+    }
+
+    public void Start()
+    {
+        OnTabSelected(TabButtons.FirstOrDefault(b => b.IsPrimary));
+    }
+
+    public void OnTabExit(TabButton tabButton)
+    {
+        ResetTabs();
+    }
+
+    public void OnTabSelected(TabButton tabButton)
+    {
+        tabButton.BackgroundImage.sprite = ActiveSprite;
+
+        SelectedButton?.Panel.Panel.SetActive(false);
+        tabButton.Panel.Panel.SetActive(true);
+
+        SelectedButton = tabButton;
+        ResetTabs();
+    }
+
+    public void ResetTabs()
+    {
+        foreach (var button in TabButtons.Where(b => b != SelectedButton))
+        {
+            button.BackgroundImage.sprite = IdleSprite;
+        }
+    }
+}
