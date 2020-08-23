18cd594d5aba67185c45c97ba67cc6bddcfb8a77
Rich Moll <drise13@gmail.com>
2020-08-23 17:18:14 -0500

    Added gridlines

--- TheDirtlot/Assets/TileController.cs
+++ TheDirtlot/Assets/TileController.cs
@@ -10,14 +10,20 @@ public class TileController : MonoBehaviour
 {
     public GameObject ContentPanel;
 
+    public List<GameObject> GridLines;
+
     public int GridSize;
+    public GameObject LinePanel;
     public List<List<Tile>> TileGrid;
     public GameObject TilePrefab;
 
     // Start is called before the first frame update
     private void Start()
     {
+        TileGrid = new List<List<Tile>>();
+        GridLines = new List<GameObject>();
         PrepareGrid();
+        DrawGrid();
     }
 
     public void PrepareGrid()
@@ -34,10 +40,40 @@ public class TileController : MonoBehaviour
                 var tile = prefab.GetComponent<Tile>();
                 tile.CurrentTool = ToolManager.Instance.Tools.First(t => t.ToolType == ToolType.Dirt);
 
+                var tileSize = tile.TileSize;
+
                 TileGrid[x].Add(tile);
 
-                prefab.transform.localPosition = new Vector3(x * tile.TileSize, -y * tile.TileSize, 0);
+                prefab.transform.localPosition = new Vector3(x * tileSize, -y * tileSize, 0);
             }
+        }
+    }
+
+    public void DrawGrid()
+    {
+        var tileSize = TileGrid.First().First().TileSize;
+
+        foreach (var gridLine in GridLines)
+        {
+            Destroy(gridLine);
+        }
+
+        foreach (var y in Enumerable.Range(1, GridSize - 1))
+        {
+            var verticalLine = Instantiate(LinePanel, ContentPanel.transform);
+            verticalLine.transform.localPosition = new Vector3(y * tileSize, 0, 5);
+            verticalLine.transform.GetComponent<RectTransform>().sizeDelta = new Vector2(5, GridSize * tileSize);
+
+            GridLines.Add(verticalLine);
+        }
+
+        foreach (var x in Enumerable.Range(1, GridSize - 1))
+        {
+            var horizontalLine = Instantiate(LinePanel, ContentPanel.transform);
+            horizontalLine.transform.localPosition = new Vector3(0, -x * tileSize, 5);
+            horizontalLine.transform.GetComponent<RectTransform>().sizeDelta = new Vector2(GridSize * tileSize, 5);
+
+            GridLines.Add(horizontalLine);
         }
     }
 
